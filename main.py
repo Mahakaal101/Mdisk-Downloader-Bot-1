aa141e6309c5c5c9b63ed68c60733c7c91d1eccc
amit4407 <110707613+amit4407@users.noreply.github.com>
2022-09-08 23:36:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ ban = os.environ.get("BAN", "")
 def echo(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
 
     if not checkuser(message):
-        app.send_message(message.chat.id, '__You are either not **Authorized** or **Contact @Aaajaats To Use this bot**__',reply_to_message_id=message.id,reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Update Channel", url="https://t.me/anumitultrabots")]]))
+        app.send_message(message.chat.id, '__You are either not **Authorized** or **Contact @Aaajats To Use this bot**__',reply_to_message_id=message.id,reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Update Channel", url="https://t.me/anumitultrabots")]]))
         return
 
     app.send_message(message.chat.id, '**Hi, I am Mdisk Video Downloader, you can watch Videos without MX Player.\n__Send me a link to Start...__**',reply_to_message_id=message.id,
@@ -43,7 +43,7 @@ def echo(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
 def help(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
     
     if not checkuser(message):
-        app.send_message(message.chat.id, '__You are either not **Authorized** or **Contact @Aaajaats To Use this bot**__',reply_to_message_id=message.id)
+        app.send_message(message.chat.id, '__You are either not **Authorized** or **Contact @Aaajats To Use this bot**__',reply_to_message_id=message.id)
         return
     
     helpmessage = """__**/start** - basic usage
