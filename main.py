cd2d321342f52d70913f863e5d6565483ce15584
amit4407 <110707613+amit4407@users.noreply.github.com>
2022-09-08 16:55:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)
 
 
 # optionals
-auth = os.environ.get("AUTH", "1864861524 5076949930")
+auth = os.environ.get("AUTH", "1864861524,5076949930")
 ban = os.environ.get("BAN", "")
 
 
