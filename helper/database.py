26d11dfed0214e9e2db3678e755d7c17ea6fefaf
Annumit <115851650+Annumit@users.noreply.github.com>
2022-12-12 15:27:36 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,8 +1,10 @@
 import pymongo 
 import os
+import datetime
+import motor.motor_asyncio
 from helper.date import add_date
 DB_NAME = os.environ.get("DB_NAME","amiami")
-DB_URL = os.environ.get("DB_URL","mongodb+srv://amit:amit@cluster0.a9eqqkf.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://amit:amit@cluster0.1je0krd.mongodb.net/?retryWrites=true&w=majority")
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["user"]
@@ -75,7 +77,45 @@ def delete(id):
 	
 def find_one(id):
 	return dbcol.find_one({"_id":id})
-	
+
+
+class Database:
+
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.clinton = self._client[database_name]
+        self.col = self.clinton.USERS
+
+    def new_user(self, id):
+        return dict(id=id, thumbnail=None)
+
+
+    async def add_user(self, id):
+        user = self.new_user(id)
+        await self.col.insert_one(user)
+
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return True if user else False
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self):
+        all_users = self.col.find({})
+        return all_users
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def set_thumbnail(self, id, thumbnail):
+        await self.col.update_one({'id': id}, {'$set': {'thumbnail': thumbnail}})
+
+    async def get_thumbnail(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('thumbnail', None)
+
     async def remove_ban(self, id):
         ban_status = dict(
             is_banned=False,
